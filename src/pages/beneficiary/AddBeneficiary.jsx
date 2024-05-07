280ba14e51cd7acce22bd13eb4a537cdba76b04f
sweta1609 <swetakumarisingh009@gmail.com>
2024-05-07 15:37:14 +0530

    adding navbar

--- src/pages/beneficiary/AddBeneficiary.jsx
+++ src/pages/beneficiary/AddBeneficiary.jsx
@@ -1,15 +1,20 @@
 import React from 'react'
 import SideBar from '../../component/sideBar/SideBar'
 import BeneficiaryForm from './BeneficiaryForm';
+import Navbar from '../../component/common/Navbar';
 
-const AddBeneficiary = () => {
+const AddBeneficiary = ({instance}) => {
   return (
     <>
       <div className="flex bg-[#0F141D] h-screen w-screen">
         <SideBar />
+        <div className="flex flex-col w-full m-1">
+        <Navbar instance={instance}/>
         <div className=''>
           <BeneficiaryForm />
         </div>
+        </div>
+        
     </div>
     </>
   )
